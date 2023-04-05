415ca87f0f3fe8f6fc431947fa718d90d7abd3c4
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-04-05 13:49:43 +0700

    Initial Commit

---
+++ PEInfo/PEInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PEInfoApp.swift
+//  PEInfo
+//
+//  Created by Venti on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PEInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
